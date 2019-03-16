6b92dda311399429b620e0449238991677f8e11f
ChetanAmrao <32968090+ChetanAmrao@users.noreply.github.com>
2019-03-16 15:57:07 +0530

    HRUSHIKESH : cleanup + encoding of team names from dict

--- IPL_MACHINE_LEARNING/initial_1.py
+++ IPL_MACHINE_LEARNING/initial_1.py
@@ -9,18 +9,19 @@ print(df.head())
 df = np.asarray(df)
 nIndex,jIndex = df.shape
 k = 0
+x = np.zeros((nIndex,4))
 for i in range(1,nIndex):
 	key = df[i,4]
 	if key in team_dict:
-		print("old key %s"%key)
+		continue
 	else:
-		print("NEW KEY %s"%key)
 		team_dict[key] = k
 		k = k + 1
-print(nIndex,jIndex)
-print(team_dict)
+for j in range(0,2):
+	for i in range(0,nIndex):
+		key = df[i,j+4]
+		x[i,j] = team_dict[key]
+		print(key,team_dict[key])
+	print("---------------------------------------------------------------\n\n\n")
 
-
-x = np.zeros((nIndex,4))
-x[:,0] = df[:,4]
 print(x)
