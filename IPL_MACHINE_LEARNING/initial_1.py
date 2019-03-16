8a8210a934d33098c21cd49a525284b1be8492ca
ChetanAmrao <32968090+ChetanAmrao@users.noreply.github.com>
2019-03-16 17:48:49 +0530

    HRUSHIKESH : encoding of winner names from dict

--- IPL_MACHINE_LEARNING/initial_1.py
+++ IPL_MACHINE_LEARNING/initial_1.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import numpy as np
 
-path = "../ipl1/matches.csv"
+path = "../ipl1/modifiedMATCHES.csv"
 team_dict = {}
 df = pd.read_csv(path)
-print(df.head())
 
 df = np.asarray(df)
 nIndex,jIndex = df.shape
 k = 0
 x = np.zeros((nIndex,4))
+y = np.zeros((nIndex,1))
 for i in range(1,nIndex):
 	key = df[i,4]
 	if key in team_dict:
@@ -24,4 +24,11 @@ for j in range(0,3):
 		print(key,team_dict[key])
 	print("---------------------------------------------------------------\n\n\n")
 
-print(x)
+for i in range(0, nIndex):
+	key = df[i,10]
+	if (key == "tie"):
+		y[i,0] = -1
+	else:
+		y[i,0] = team_dict[key]
+
+print(y)
